76298f48b4c28fe513dfbd0c7463e305e46af618
Anthony <internet@anthonyaubree.com>
2015-06-29 11:20:42 +0200

    Début de front-end : navbar + header avec Bootstrap. Implémentation basique des controllers angularJS + définition des routes (à modifier si nécessaire par la suite).

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,39 @@
 var app = angular.module("geektic", ['ngRoute']);
 
-app.controller('HelloCtrl', function($scope, $http) {
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'welcome.html',
+                controller: 'IndexCtrl'
+            }).
+            when('/geek/:id', {
+                templateUrl: 'geek_details.html',
+                controller: 'GeekCtrl'
+            }).
+            when('/resultats', {
+                templateUrl: 'results.html',
+                controller: 'ResultsCtrl'
+            })
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
+app.controller('IndexCtrl', function($scope, $http) {
     $http.get('/api/hello').success(function(helloMessage) {
         $scope.helloMessage = helloMessage;
+    });
+});
+
+app.controller('ResultsCtrl', function($scope, $http) {
+    $http.get('/api/resultats').success(function(showResults) {
+        $scope.showResults = showResults;
+    });
+});
+
+app.controller('GeekCtrl', function($scope, $http, $routeParams) {
+    $http.get('api/geek/' + $routeParams.id).success(function (showGeek) {
+        $scope.showGeek = showGeek;
     });
 });
