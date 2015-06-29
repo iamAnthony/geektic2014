488865ce402314a1779939da2d36919a823bccf9
Anthony <internet@anthonyaubree.com>
2015-06-29 15:44:53 +0200

    Création du service Geek + fonction de récupération des détails d'un Geek par son ID : geekDetails(id). Correction des routes angular. Affichage des détails d'un geek (geek_details.html)

---
+++ src/main/java/com/ninja_squad/geektic/service/GeekService.java
@@ -0,0 +1,31 @@
+package com.ninja_squad.geektic.service;
+
+import com.ninja_squad.geektic.dao.GeekTicDao;
+import com.ninja_squad.geektic.model.Geek;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * Created by Anthony on 29/06/15.
+ * Project : geektic2014
+ */
+
+@RestController
+@RequestMapping("/geek")
+public class GeekService {
+
+    @Autowired
+    private GeekTicDao dao;
+
+    @RequestMapping(method = GET, value="/{id}")
+    public Geek geekDetails(@PathVariable("id") int id) {
+        return dao.findById(id);
+    }
+}
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -5,7 +5,6 @@ app.config(['$routeProvider',
         $routeProvider.
             when('/', {
                 templateUrl: 'welcome.html',
-                controller: 'IndexCtrl'
             }).
             when('/geek/:id', {
                 templateUrl: 'geek_details.html',
@@ -14,26 +13,20 @@ app.config(['$routeProvider',
             when('/resultats', {
                 templateUrl: 'results.html',
                 controller: 'ResultsCtrl'
-            })
+            }).
             otherwise({
                 redirectTo: '/'
             });
     }]);
 
-app.controller('IndexCtrl', function($scope, $http) {
-    $http.get('/api/hello').success(function(helloMessage) {
-        $scope.helloMessage = helloMessage;
-    });
-});
-
 app.controller('ResultsCtrl', function($scope, $http) {
-    $http.get('/api/resultats').success(function(showResults) {
+    $http.get('/resultats').success(function(showResults) {
         $scope.showResults = showResults;
     });
 });
 
 app.controller('GeekCtrl', function($scope, $http, $routeParams) {
-    $http.get('api/geek/' + $routeParams.id).success(function (showGeek) {
+    $http.get('/geek/' + $routeParams.id).success(function (showGeek) {
         $scope.showGeek = showGeek;
     });
 });
